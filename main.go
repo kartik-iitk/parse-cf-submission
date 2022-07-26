c18c2cc13de98353b1ea0b799aa72474edcc5b46
kartik-iitk <kartik21@iitk.ac.in>
2022-07-27 00:21:51 +0530

    Successfully Parsed
    Completed webscraping a random html file of a codeforces submission to extract relevant information.

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type info struct {
+	submissionID string
+	author       string
+	problem      string
+	language     string
+	verdict      string
+	code         string
+}
+
+func scrapeInfo(doc *goquery.Document) *info {
+	// Method to find selector for find: go to browser, use inspect element
+	// feature, and hover over required portion of the html.
+	// Next, see the required class-type of immediate or close-by parents and
+	// try searching from there directly, you need not start from the class of
+	// the very first parent! Classes and ids are what you should look out for
+	// while scraping webpages.
+	// How to test selector? Use the console, to write jQueries, and see if you
+	// are getting the correct output. functions like alert, innerText,
+	// specifying index simply by [...] after selector (...) all are helpful.
+	// The $(..) command can only be used if
+	// your website uses jQueries (which in most cases it does) else you have
+	// to actually write jQueries!
+	var data info
+
+	// Find everything other than the code.
+	doc.Find(".datatable div table tr").Eq(1).Each(
+		// Find the class datatable (you don't need to start from .body)
+		// and then find list of div tags, then find amongst them
+		// the ones with table in them, in the new list find the tr tags and
+		// Each() will iterate over all the rows, but Eq(1) will restrict
+		// into the second row (provides index).
+		func(index int, item *goquery.Selection) {
+			// In the row, iterate over all columns (td tags).
+			doc.Find("td").Each(
+				func(index int, item *goquery.Selection) {
+					value := item.Text()
+					switch index {
+					case 0:
+						data.submissionID = strings.TrimSpace(value)
+					case 1:
+						data.author = strings.TrimSpace(value)
+					case 2:
+						data.problem = strings.TrimSpace(value)
+					case 3:
+						data.language = strings.TrimSpace(value)
+					case 4:
+						data.verdict = strings.TrimSpace(value)
+					}
+				})
+		})
+
+	// Find the code. find should ideally only return 1 object, but to access
+	// it we use index 0.
+	codeNode := doc.Find(".prettyprint").Eq(0)
+	data.code = codeNode.Text()
+
+	return &data
+}
+
+func main() {
+	res, err := http.Get("https://codeforces.com/contest/1696/submission/161733082")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close() // deferred closing statement.
+	if res.StatusCode > 400 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document.
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := scrapeInfo(doc)
+	fmt.Println(*data)
+}
